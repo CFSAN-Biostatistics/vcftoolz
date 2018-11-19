5749dbbad257e789d870222e04f934ebccb77182
Steve Davis <steven.davis@fda.hhs.gov>
2018-11-19 16:15:19 -0500

    Flake8 compliance.

--- tests/test_vcftoolz.py
+++ tests/test_vcftoolz.py
@@ -73,5 +73,5 @@ def test_get_set_intersections():
     of the sets.  If the original list has N sets, the returned list will
     have (2**N)-1 sets.
     """
-    intersections = vcftoolz.get_set_intersections([{"100","110","101","111"}, {"110","010","111","011"}, {"101","111","011","001"}])
+    intersections = vcftoolz.get_set_intersections([{"100", "110", "101", "111"}, {"110", "010", "111", "011"}, {"101", "111", "011", "001"}])
     assert(intersections == [('111', {'111'}), ('011', {'011'}), ('101', {'101'}), ('001', {'001'}), ('110', {'110'}), ('010', {'010'}), ('100', {'100'})])
