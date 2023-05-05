5e8c135cccb8c28e6c889f91af30b90e277b025d
Justin Payne <crashfrog@gmail.com>
2023-05-05 12:39:36 +0700

    dumb bug

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ setup(
     version='1.2.1',
     description="Tools for working with Variant Call Format files.",
     long_description=readme + '\n\n' + history,
-    author="Steve Davis", "Justin Payne"
-    author_email='steven.davis@fda.hhs.gov', 'justin.payne@fda.hhs.gov'
+    author=["Steve Davis", "Justin Payne"],
+    author_email=['steven.davis@fda.hhs.gov', 'justin.payne@fda.hhs.gov'],
     url='https://github.com/CFSAN-Biostatistics/vcftoolz',
     packages=[
         'pyvenn',
